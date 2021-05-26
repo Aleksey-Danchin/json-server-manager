491cdf0e5854f3e9090b09e707b7a04007564dea
T1M0FE1 <timofei.magi@gmail.com>
2021-05-26 03:50:29 +0300

    Update users.js
    Add patch route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,17 +2,19 @@ import express from "express";
 const app = express.Router();
 
 app.get("/users", (req, res) => {
-  res.status(200).send([ /* ARRAY OF USERS */]);
+  res.status(200).send([ /* ARRAY OF USERS */ ]);
 });
 
 app.get("/users/:id", (req, res) => {
-  res.status(200).send({ /* USER DATA */});
+  res.status(200).send({ /* USER DATA */ });
+});
+
+app.patch("/users/:id", (res, res) => {
+  res.status(200).send({ /* UPDATED USER DATA */ });
 });
 
 app.post("/users", (req, res) => {
-  res.status(201).send({
-    // created user data
-  });
+  res.status(201).send({ /* CREATED USER DATA */ });
 });
 
 app.delete("/users/:id", (req, res) => {
